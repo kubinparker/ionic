b8655f3008e33087db9a7cda21c176911582ca4b
Nguyen Thanh Huy <huy.nguyenthanh@caters.co.jp>
2021-09-22 19:00:43 +0900

    header

--- jpage/src/pages/Home.tsx
+++ jpage/src/pages/Home.tsx
@@ -1,54 +1,52 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonCheckbox, IonLabel, IonNote, IonBadge, IonFab, IonFabButton, IonIcon } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonButtons, IonToolbar, IonFab, IonFabButton, IonIcon, IonButton, IonAvatar, IonImg, IonItem, IonThumbnail, IonSearchbar, IonInput } from '@ionic/react';
 // import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
-import { add } from 'ionicons/icons';
+import { add, personCircle, search } from 'ionicons/icons';
 import { RouteComponentProps } from 'react-router';
+import { useState } from 'react';
 // import { attachProps } from '@ionic/react/dist/types/components/utils';
 
 const Home: React.FC<RouteComponentProps> = (props) => {
+	const [isSearchBarOpened, setIsSearchBarOpened] = useState<string>("ion-hide");
 	return (
 		<IonPage>
 
 			<IonHeader>
 				<IonToolbar>
-					<IonTitle>Blank Browser</IonTitle>
+					{/* <IonItem class="logo" slot="start">
+						<IonThumbnail >
+							<IonImg src="/assets/icon/jpage_icon.png" alt='' />
+						</IonThumbnail>
+					</IonItem> */}
+					<IonButtons slot="start" class="logo">
+						<IonThumbnail >
+							<IonImg src="/assets/icon/jpage_icon.png" alt='' />
+						</IonThumbnail>
+					</IonButtons>
+					<IonSearchbar class={isSearchBarOpened}></IonSearchbar>
+					<IonButtons slot="end">
+						<IonInput></IonInput>
+						<IonButton>
+
+							<IonIcon slot="icon-only" icon={search} />
+						</IonButton>
+						<IonButton>
+							<IonIcon slot="icon-only" icon={personCircle} />
+						</IonButton>
+					</IonButtons>
 				</IonToolbar>
-			</IonHeader>
+
+			</IonHeader >
 
 			<IonContent fullscreen>
-
-				<IonList>
-					<IonItem>
-						<IonCheckbox slot="start" />
-						<IonLabel>
-							<h1>Create Idea</h1>
-							<IonNote>Run Idea by Brandy</IonNote>
-						</IonLabel>
-						<IonBadge color="success" slot="end">
-							5 Days
-						</IonBadge>
-					</IonItem>
-					<IonItem>
-						<IonCheckbox slot="start" />
-						<IonLabel>
-							<h1>Create Idea</h1>
-							<IonNote>Run Idea by Brandy</IonNote>
-						</IonLabel>
-						<IonBadge color="success" slot="end">
-							5 Days
-						</IonBadge>
-					</IonItem>
-				</IonList>
-
-				<IonFab vertical="bottom" horizontal="end" slot="fixed">
+				{/* <IonFab vertical="bottom" horizontal="end" slot="fixed">
 					<IonFabButton onClick={() => props.history.push('/new')}>
 						<IonIcon icon={add} />
 					</IonFabButton>
-				</IonFab>
-
+				</IonFab> */}
 			</IonContent>
 
-		</IonPage>
+		</IonPage >
 	);
 };
 
